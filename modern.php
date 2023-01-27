ad95ae27f2f54ddf7d8dca67282b0c5ceacb2aba
Marlvern <marlvernchimbwanda1002@gmail.com>
2023-01-27 12:41:36 +0200

    trying some new login design

---
+++ modern.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bo">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "box" >
+        <div class = "container">
+            <div class="topheader">
+                <span>Have an account?</span>
+                <header>Login</header>
+            </div>
+
+            <div class="input-field">
+                <i class = "bx bx-user"> </i>
+                <input type="text"  class = "input" placeholder = "username" required>                
+            </div>
+
+            <div class="input-field">
+                <i class = "bx bx-lock-alt"></i>
+                <input type="password"  class = "input" placeholder = "Password" required>                
+            </div>
+
+            <div class="input-field">
+                <input type="submit"  class = "submit" value = "Login" >                
+            </div>
+
+            <div class="bottom">
+
+                <div class="left">
+                <input type="checkbox" id="check">
+                <label for="check">Remember Me</label>
+                </div>
+
+                <div class="right">
+                    <label> <a href="#">Forgort Password?</a></label>
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+    </div>
+</body>
+</html>
